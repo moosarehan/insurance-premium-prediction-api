df8e33a76bee33d23d900c880bb65756bf9f0b12
moosarehan <moosarehan4@gmail.com>
2026-07-17 12:11:40 +0500

    feat(api): integrate schemas and model prediction endpoints in app.py

--- app.py
+++ app.py
@@ -1,95 +1,40 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field, computed_field,field_validator
-from typing import Literal, Annotated
-import pickle
-import pandas as pd
-
-with open('model/model.pkl', 'rb') as f:
-    model = pickle.load(f)
-
+from schema.userinput import UserInput
+from model.predict import predict_output,model
+from model.predict import MODEL_VERSION
+from schema.prediction_response import PredictionResponse
 app = FastAPI()
 
-tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
-tier_2_cities = [
-    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
-    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
-    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
-    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
-    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
-    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
-]
-
-class UserInput(BaseModel):
-    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
-    weight: Annotated[float, Field(..., gt=0, description='Weight of the user')]
-    height: Annotated[float, Field(..., gt=0, lt=2.5, description='Height of the user')]
-    income_lpa: Annotated[float, Field(..., gt=0, description='Annual salary of the user in lpa')]
-    smoker: Annotated[bool, Field(..., description='Is user a smoker')]
-    city: Annotated[str, Field(..., description='The city that the user belongs to')]
-    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
-                                   'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
-    
-    @computed_field
-    @property
-    def bmi(self) -> float:
-        return self.weight / (self.height ** 2)
-    
-    @computed_field
-    @property
-    def lifestyle_risk(self) -> str:
-        if self.smoker and self.bmi > 30:
-            return "high"
-        elif self.smoker or self.bmi > 27:
-            return "medium"
-        else:
-            return "low"
-        
-    @computed_field
-    @property
-    def age_group(self) -> str:
-        if self.age < 25:
-            return "young"
-        elif self.age < 45:
-            return "adult"
-        elif self.age < 60:
-            # NOTE: If your model was trained on "middle aged" with a space, change this to "middle aged"
-            return "middle_aged" 
-        return "senior"
-    
-    @computed_field
-    @property
-    def city_tier(self) -> int:
-        if self.city in tier_1_cities:
-            return 1
-        elif self.city in tier_2_cities:
-            return 2
-        else:
-            return 3
-    @field_validator('city')
-    @classmethod
-    def normalize(cls,val:str)->str:
-        return val.strip().title()
 
 @app.get('/')
 def welcome():
     return{'messsage':'Welcome to Insurance premium prediction API'}
 
-@app.post('/predict')
+@app.get('/healthcheck')
+def health():
+    return{
+        'status':'Ok',
+        'model version':MODEL_VERSION
+    }
+@app.post('/predict',response_model=PredictionResponse)
 def predict_premium(data: UserInput):
-    input_df = pd.DataFrame([{
+    user_input= {
         'bmi': data.bmi,
         'age_group': data.age_group,
         'lifestyle_risk': data.lifestyle_risk,
         'city_tier': data.city_tier,
         'income_lpa': data.income_lpa,
         'occupation': data.occupation
-    }])
+    }
 
     # 1. Get prediction
-    prediction = model.predict(input_df)[0]
+    try:
+        prediction=predict_output(user_input)
+        return JSONResponse(status_code=200, content=prediction)
+    except Exception as e:
+        return JSONResponse(status_code=500,content=str(e))
+   
 
-    # 2. Convert prediction to standard Python type (str) to avoid silent JSON serialization failure
-    serializable_prediction = str(prediction)
 
-    return JSONResponse(status_code=200, content={'predicted_category': serializable_prediction})
+   
