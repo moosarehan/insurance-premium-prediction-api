d148073dcf96ae3024ff1109655b5e842807e7a8
moosarehan <moosarehan4@gmail.com>
2026-07-17 12:10:35 +0500

    feat(model): add prediction pipeline to load model and process inputs

---
+++ model/predict.py
@@ -0,0 +1,20 @@
+
+import pickle
+import pandas as pd
+
+with open('model/model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+MODEL_VERSION='1.0.0'
+class_labels = model.classes_.tolist()
+def predict_output(userinput:dict):
+    input_df=pd.DataFrame([userinput])
+    prediction = model.predict(input_df)[0]
+    probabilities=model.predict_proba(input_df)[0]
+    confidence=max(probabilities)
+    class_probs=dict(zip(class_labels,map(lambda p:round(p,4),probabilities)))
+    return {
+        'predicted_category':prediction,
+        'confidence':round(confidence,4),
+        'class_probabilities':class_probs
+    }
